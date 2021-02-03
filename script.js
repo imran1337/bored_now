f7bdaa84abfd27eeb939dba82359180adf29b480
Coding Bird <Codingbird@yahoo.com>
2021-02-04 11:19:28 +1200

    some logic added

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-function handleActivity() {
+function storeActivity() {
   fetch(`https://www.boredapi.com/api/activity`)
     .then((res) => res.json())
     .then((data) => localStorage.setItem("activity", JSON.stringify(data)))
     .catch((err) => console.log(err));
-  //show activity
+    showData();
+}
+
+// show data
+function showData() {
   const activityDetails = JSON.parse(localStorage.getItem("activity"));
+  if(activityDetails) {
   const activityType = document.getElementById("activity_type");
   const activityBody = document.getElementById("activity_text");
   activityType.innerText = activityDetails.type;
   activityBody.innerText = activityDetails.activity;
-//   link show
-  learnLink(activityDetails.link)
+  //   link show
+  learnLink(activityDetails.link);
+  } else{
+    console.log('local storage no data');
+  }
 }
 
 // link
 function learnLink(link) {
-    const learnLink = document.createElement("p");
-  if(link) {
+  const learnLink = document.createElement("p");
+  if (link) {
     learnLink.innerHTML = `<a href=${link} target=__black>Learn here</a>`;
     document.getElementById("activity_text").appendChild(learnLink);
-  } else{
-      console.log("not link");
+  } else {
+    console.log("not link");
   }
 }
 
-// handleActivity(); // <-- default call for refresh to activity show
-document.getElementById("nextActivity").addEventListener("click", handleActivity);
+showData(); // <-- default call for refresh to activity show
+document.getElementById("nextActivity").addEventListener("click", storeActivity);
